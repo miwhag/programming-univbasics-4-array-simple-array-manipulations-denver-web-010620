4765eef3206ea49e8e33636b9b8baa0eddec9cba
Miwha Geschwind <miwhag@gmail.com>
2019-12-21 16:55:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -49,10 +49,9 @@ shift_with_args(["one","two","three"])
 
 
 def using_concat(arr1,arr2)
-[arr1].concat([arr2])
+arr1.concat( [arr2] )
 end
 
 using_concat(["one","two","three"], ["four","five","six"])
-
 
 
